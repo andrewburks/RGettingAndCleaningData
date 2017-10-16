d7eae34d8f8f3e2c144a42f2825c2d7abc2eb1db
Andrew Burks <andrew.burks@gmail.com>
2017-10-15 22:54:51 -0500

    Files for the course final project

---
+++ run_analysis.R
@@ -0,0 +1,45 @@
+library(dplyr)
+
+# Set the root directory to where data archive was unzipped.
+dataRootDir <- "UCI HAR Dataset"
+
+
+# Read the activity names.
+activityNames <- 
+    read.table(paste(dataRootDir, "activity_labels.txt", sep = "/"), col.names = c("Activity.Id", "Activity.Name"))
+
+
+# Read and clean up the variable names into column worthy names.
+variableNames <- 
+    read.table(paste(dataRootDir, "features.txt", sep = "/"), col.names = c("Variable.Id", "Variable.Name.Raw")) %>% 
+    mutate(Variable.Name =  gsub("-|,", ".", gsub("\\(\\)", "", Variable.Name.Raw))) %>% 
+    select(Variable.Id, Variable.Name)
+
+
+# A function for reading in and assembling one of these datasets from the structured data.
+readDataset <- function(datasetName) {
+    # Read the subject id column.
+    subjectFilePath <- file.path(dataRootDir, datasetName, paste("subject_", datasetName, ".txt", sep=""))
+    subjectIds <- read.table(subjectFilePath, col.names = c("Subject.Id"))
+    
+    # Read the activity id column, merge in the activity names and drop the activity id
+    activityFilePath <- file.path(dataRootDir, datasetName, paste("y_", datasetName, ".txt", sep=""))
+    activityIds <- read.table(activityFilePath, col.names = c("Activity.Id"))
+    activities <- merge(activityIds, activityNames) %>% select(Activity.Name)
+    
+    # Read the variable values, filtering for only "mean" and "std" columns
+    variableDataFilePath <- file.path(dataRootDir, datasetName, paste("X_", datasetName, ".txt", sep=""))
+    variableData <- read.table(variableDataFilePath, col.names = variableNames$Variable.Name)
+    variableData <- variableData[, grep("\\.mean\\.|\\.std\\.", names(variableData))]
+    
+    # Bind the columns together, returning the resulting data frame
+    cbind(subjectIds, activities, variableData)
+}
+
+trainData <- readDataset("train")
+
+testData <- readDataset("test")
+
+# Compbine the train and test data sets and calculate averages grouped subject and activity
+finalData <- rbind(trainData, testData) %>% group_by(Subject.Id, Activity.Name) %>% summarise_all(funs(mean))
+
