fee45a764aca39f668dee853187d62849788756f
Andrew Burks <andrew.burks@gmail.com>
2017-10-15 23:11:51 -0500

    Typos and final file write

--- run_analysis.R
+++ run_analysis.R
@@ -1,24 +1,24 @@
 library(dplyr)
 
-# Set the root directory to where data archive was unzipped.
+# Set the root directory to where data archive was unzipped
 dataRootDir <- "UCI HAR Dataset"
 
 
-# Read the activity names.
+# Read the activity names
 activityNames <- 
     read.table(paste(dataRootDir, "activity_labels.txt", sep = "/"), col.names = c("Activity.Id", "Activity.Name"))
 
 
-# Read and clean up the variable names into column worthy names.
+# Read and clean up the variable names into column worthy names
 variableNames <- 
     read.table(paste(dataRootDir, "features.txt", sep = "/"), col.names = c("Variable.Id", "Variable.Name.Raw")) %>% 
     mutate(Variable.Name =  gsub("-|,", ".", gsub("\\(\\)", "", Variable.Name.Raw))) %>% 
     select(Variable.Id, Variable.Name)
 
 
-# A function for reading in and assembling one of these datasets from the structured data.
+# A function for reading in and assembling one of these datasets from the structured data
 readDataset <- function(datasetName) {
-    # Read the subject id column.
+    # Read the subject id column
     subjectFilePath <- file.path(dataRootDir, datasetName, paste("subject_", datasetName, ".txt", sep=""))
     subjectIds <- read.table(subjectFilePath, col.names = c("Subject.Id"))
     
@@ -40,6 +40,9 @@ trainData <- readDataset("train")
 
 testData <- readDataset("test")
 
-# Compbine the train and test data sets and calculate averages grouped subject and activity
+# Combine the train and test data sets and calculate averages grouped subject and activity
 finalData <- rbind(trainData, testData) %>% group_by(Subject.Id, Activity.Name) %>% summarise_all(funs(mean))
+
+# Write out the final data
+write.table(finalData, "final_data.txt", row.names = FALSE)
 
